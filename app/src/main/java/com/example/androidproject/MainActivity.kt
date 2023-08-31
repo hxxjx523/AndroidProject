6ed4fe9b89438dd0177de92345819cab723ce3d7
heeju <hhj05230@gmail.com>
2023-08-31 19:07:28 +0900

    test

---
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        //깃 연결 테스트
+    }
+}
