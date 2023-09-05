01feb9aed14a6f0b91acb10dfdc71302f6301c35
heeju <hhj05230@gmail.com>
2023-09-05 18:53:24 +0900

    메인 화면 완성

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main)
         
         //깃 연결 테스트
     }
