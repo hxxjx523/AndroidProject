c8c9d1e44abf861be9122882250a81e3b599c0a5
heeju <hhj05230@gmail.com>
2023-10-12 18:42:06 +0900

    android

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.androidproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        
-        //깃 연결 테스트
+
+        val btn1 = findViewById<Button>(R.id.btn1)
+
+        btn1.setOnClickListener({
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
