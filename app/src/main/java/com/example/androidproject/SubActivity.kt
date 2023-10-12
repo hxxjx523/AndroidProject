d8cadaaca3206db1f871d734afb7603bb4361c44
heeju <hhj05230@gmail.com>
2023-10-12 19:50:10 +0900

    android

--- app/src/main/java/com/example/androidproject/SubActivity.kt
+++ app/src/main/java/com/example/androidproject/SubActivity.kt
@@ -1,13 +1,20 @@
 package com.example.androidproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
 
-//        val api_key =
+        val Temperature_choice = findViewById<Button>(R.id.Temperature_choice)
+
+        Temperature_choice.setOnClickListener({
+            val intent = Intent(this, Clothes::class.java)
+            startActivity(intent)
+        })
     }
 }
